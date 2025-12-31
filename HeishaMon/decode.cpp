f027d47cf13e2b80b4db6ac8292cc9b3c1548a4d
ghecker1 <79323013+ghecker1@users.noreply.github.com>
2025-12-31 21:59:17 +0100

    fix decoding demand control

--- HeishaMon/decode.cpp
+++ HeishaMon/decode.cpp
@@ -273,7 +273,7 @@ String getOptDataValue(char* data, unsigned int Topic_Number) {
       Topic_Value = String((data[5] >> 0) & 0b1);
       break;
     case 7:
-      Topic_Value = String(optionalPCBQuery[14]);
+      Topic_Value = String(data[14]);
       break;
     default:
       break;
@@ -383,14 +383,20 @@ void decode_heatpump_data_extra(char* data, char* actDataExtra, PubSubClient &mq
 void decode_optional_heatpump_data(char* data, char* actOptData, PubSubClient & mqtt_client, void (*log_message)(char*), char* mqtt_topic_base, unsigned int updateAllTime) {
   bool updateTime = false;
   bool updateTopic[NUMBER_OF_OPT_TOPICS] = { false };
+  char tmpData[OPTDATASIZE] = { '\0' };
 
   if ((lastalloptdatatime == 0) || ((unsigned long)(millis() - lastalloptdatatime) > (1000 * updateAllTime))) {
     updateTime = true;
     lastalloptdatatime = millis();
   }
+
+  // Use tmpData with DemandControl included
+  memcpy(tmpData, data, OPTDATASIZE);
+  tmpData[14] = optionalPCBQuery[14];
+
   for (unsigned int Topic_Number = 0 ; Topic_Number < NUMBER_OF_OPT_TOPICS ; Topic_Number++) {
     String Topic_Value;
-    Topic_Value = getOptDataValue(data, Topic_Number);
+    Topic_Value = getOptDataValue(tmpData, Topic_Number);
 
     if(getOptDataValue(actOptData, Topic_Number) != Topic_Value) {
       updateTopic[Topic_Number] = true;
@@ -407,14 +413,12 @@ void decode_optional_heatpump_data(char* data, char* actOptData, PubSubClient & 
     }
   }
   //response to heatpump should contain the data from heatpump on byte 4 and 5
-  byte valueByte4 = data[4];
+  byte valueByte4 = tmpData[4];
   optionalPCBQuery[4] = valueByte4;
-  byte valueByte5 = data[5];
+  byte valueByte5 = tmpData[5];
   optionalPCBQuery[5] = valueByte5;
 
-  memcpy(actOptData, data, OPTDATASIZE);
-  // DemandControl
-  actOptData[14] = optionalPCBQuery[14];
+  memcpy(actOptData, tmpData, OPTDATASIZE);
   for (unsigned int Topic_Number = 0 ; Topic_Number < NUMBER_OF_OPT_TOPICS ; Topic_Number++) {
     if(updateTopic[Topic_Number]) {
       char log_msg[256];
