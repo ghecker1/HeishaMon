b9c5d3c596bb869e13ba8710c146edbeea7b9279
ghecker1 <79323013+ghecker1@users.noreply.github.com>
2026-01-15 22:31:22 +0100

    Add SET43 - SetHeatingMode

--- HeishaMon/commands.cpp
+++ HeishaMon/commands.cpp
@@ -962,6 +962,29 @@ unsigned int set_optional_pcb(char *msg, unsigned char *cmd, char *log_msg) {
   return sizeof(panasonicSendQuery);
 }
 
+unsigned int set_heating_mode(char *msg, unsigned char *cmd, char *log_msg) {
+
+  const byte address=28;
+  byte value = 0b01;
+
+  if ( String(msg).toInt() == 1 ) {
+    value = 0b10;
+  }
+
+  {
+    char tmp[256] = { 0 };
+    snprintf_P(tmp, 255, PSTR("set heating mode %d"), value - 1);
+    memcpy(log_msg, tmp, sizeof(tmp));
+  }
+
+  {
+    memcpy_P(cmd, panasonicSendQuery, sizeof(panasonicSendQuery));
+    cmd[address] = value;
+  }
+
+  return sizeof(panasonicSendQuery);
+}
+
 unsigned int set_external_compressor_control(char *msg, unsigned char *cmd, char *log_msg){
   const byte off_state=64;
   const byte address=23;
--- HeishaMon/commands.h
+++ HeishaMon/commands.h
@@ -70,6 +70,7 @@ unsigned int set_heatingcontrol(char *msg, unsigned char *cmd, char *log_msg);
 unsigned int set_smart_dhw(char *msg, unsigned char *cmd, char *log_msg);
 unsigned int set_quiet_mode_priority(char *msg, unsigned char *cmd, char *log_msg);
 unsigned int set_optional_pcb(char *msg, unsigned char *cmd, char *log_msg);
+unsigned int set_heating_mode(char *msg, unsigned char *cmd, char *log_msg);
 
 //optional pcb commands
 unsigned int set_heat_cool_mode(char *msg, char *log_msg);
@@ -161,6 +162,7 @@ const cmdStruct commands[] PROGMEM = {
   { "SetSmartDHW", set_smart_dhw },
   { "SetQuietModePriority", set_quiet_mode_priority },
   { "SetOptionalPCB", set_optional_pcb },
+  { "SetHeatingMode", set_heating_mode },
 };
 
 struct optCmdStruct{
