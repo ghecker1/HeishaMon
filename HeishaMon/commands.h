3c0af76bf167ac79854b02b475ee774f60733fa8
ghecker1 <79323013+ghecker1@users.noreply.github.com>
2026-01-15 22:30:55 +0100

    optionalpcb query interval 7000ms instead of 1000ms
    Observed random H74 errors and tried to reduce the interval to lower
    over all load on the heishamon.
    
    Running this without issues for 3 weeks now.
    
    Environment: Large heishamon + ethernet, wifi disabled.

--- HeishaMon/commands.h
+++ HeishaMon/commands.h
@@ -11,7 +11,7 @@ extern byte panasonicQuery[PANASONICQUERYSIZE];
 
 
 #define OPTDATASIZE 20
-#define OPTIONALPCBQUERYTIME 1000 //send optional pcb query each second
+#define OPTIONALPCBQUERYTIME 7000
 #define OPTIONALPCBQUERYSIZE 19
 #define OPTIONALPCBSAVETIME 300 //save each 5 minutes the current optional pcb state into flash to have valid values during reboot
 extern byte optionalPCBQuery[OPTIONALPCBQUERYSIZE];
