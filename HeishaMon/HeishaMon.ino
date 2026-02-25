8a5dc33c7c8bd49a305f89424b5e43bd2d785138
ghecker1 <79323013+ghecker1@users.noreply.github.com>
2026-02-25 23:49:57 +0100

    yield() instead of delay()

--- HeishaMon/HeishaMon.ino
+++ HeishaMon/HeishaMon.ino
@@ -1641,18 +1641,18 @@ void loop() {
   //webserver function
   statistics_start(STATISTICS_WEBSERVER_LOOP);
   webserver_loop();
-  delay(1);
+  yield() // FIX;
   statistics_end(STATISTICS_WEBSERVER_LOOP);
 
   // check wifi
   check_wifi();
-  delay(1);
+  yield() // FIX;
   // Handle OTA first.s
   ArduinoOTA.handle();
 
   //statistics_start(STATISTICS_MQTT_LOOP);
   mqtt_client.loop();
-  delay(1);
+  yield() // FIX;
   //statistics_end(STATISTICS_MQTT_LOOP);
 
   if (heishamonSettings.opentherm) {
@@ -1660,7 +1660,7 @@ void loop() {
   }
 
   readHeatpump();
-  delay(1);
+  yield() // FIX;
   #ifdef ESP32
   if (heishamonSettings.proxy) readProxy();
   #endif
@@ -1669,7 +1669,7 @@ void loop() {
     log_message(_F("Sending command from buffer"));
     popCommandBuffer();
   }
-  delay(1);
+  yield() // FIX;
 
   if (heishamonSettings.use_1wire) dallasLoop(mqtt_client, log_message, heishamonSettings.mqtt_topic_base);
 
@@ -1687,7 +1687,7 @@ void loop() {
       }
     }
   }
-  delay(1);
+  yield() // FIX;
 
   // run the data query only each WAITTIME
   if ((unsigned long)(millis() - lastRunTime) > (1000 * heishamonSettings.waitTime)) {
@@ -1808,7 +1808,7 @@ void loop() {
     stats += F("}");
     sprintf_P(mqtt_topic, PSTR("%s/stats"), heishamonSettings.mqtt_topic_base);
     mqtt_client.publish(mqtt_topic, stats.c_str(), MQTT_RETAIN_VALUES);
-    delay(1);
+    yield() // FIX;
 
     //websocket stats
 #ifdef ESP32
@@ -1839,7 +1839,7 @@ void loop() {
 #endif
     
     websocket_write_all(log_msg, strlen(log_msg));        
-    delay(1);
+    yield() // FIX;
 
     //get new data
     if (!heishamonSettings.listenonly) send_panasonic_query();
@@ -1847,7 +1847,7 @@ void loop() {
     //Make sure the LWT is set to Online, even if the broker have marked it dead.
     sprintf_P(mqtt_topic, PSTR("%s/%s"), heishamonSettings.mqtt_topic_base, mqtt_willtopic);
     mqtt_client.publish(mqtt_topic, "Online");
-    delay(1);
+    yield() // FIX;
 
 #ifdef ESP8266
     if (WiFi.isConnected()) {
@@ -1857,8 +1857,8 @@ void loop() {
   }
 
   timerqueue_update();
-  delay(1);
+  yield() // FIX;
   #ifdef ESP32
-  delay(1); // to keep watchdog happy
+  yield() // FIX; // to keep watchdog happy
   #endif
 }
